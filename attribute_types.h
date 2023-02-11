2f608e76bbb5a68b2229a92761bf179d43f7fa32
raymond <raymnzd@gmail.com>
2023-02-11 18:14:51 -0500

    add is_primary_key to attribute struct

--- attribute_types.h
+++ attribute_types.h
@@ -1,13 +1,14 @@
 #ifndef ATTRIBUTE_TYPES_H
 #define ATTRIBUTE_TYPES_H
 
-enum ATTRIBUTE_TYPE { INTEGER, BOOL, DOUBLE, CHAR, VARCHAR };
+enum ATTRIBUTE_TYPE { INTEGER, BOOL, DOUBLE, CHAR, VARCHAR, INVALID_ATTR };
 typedef enum ATTRIBUTE_TYPE ATTRIBUTE_TYPE;
 
 struct attribute {
   char *name;          // name of attribute
   ATTRIBUTE_TYPE type; // type of attribute
   int len;             // len if char/varchar
+  bool is_primary_key; // if is primarykey
 };
 
 typedef struct attribute Attribute;
